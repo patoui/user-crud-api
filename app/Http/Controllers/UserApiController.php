bf01447651f5518624e62d617b88e164d5d2c98e
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-05-27 12:32:14 -0400

    Removed unused method

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -9,16 +9,6 @@ use Illuminate\Http\Request;
 class UserApiController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
