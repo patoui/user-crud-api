f41ab764c1f7ebb1108d10748711cb05c18263d4
Patrique Ouimet <patrique.ouimet@gmail.com>
2018-05-27 09:45:25 -0400

    Added equivalent migrations and seeders for testing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserRoleSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(UserAddressSeeder::class);
     }
 }
